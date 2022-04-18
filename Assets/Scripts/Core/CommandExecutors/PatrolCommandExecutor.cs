624b485e31cb73f142113711e29c3dc9185facaa
Andrey <gitlerkaputs@yandex.ru>
2022-04-18 05:21:10 +0300

    Homework 3 and repo-reint
    Thanks for the "sanctions" b$#@!

---
+++ Assets/Scripts/Core/CommandExecutors/PatrolCommandExecutor.cs
@@ -0,0 +1,11 @@
+﻿using Abstractions.Commands.CommandsInterfaces;
+using UnityEngine;
+
+namespace Abstractions.Commands.CommandExecutors
+{
+    public class PatrolCommandExecutor : CommandExecutorBase<IPatrolCommand>
+    {
+        public override void ExecuteSpecificCommand(IPatrolCommand command)
+            => Debug.Log($"{this.name} patroling from command.StartPosition to {command.FinishPosition}");
+    }
+}
---
+++ Assets/Scripts/Core/MainUnit.cs
@@ -0,0 +1,23 @@
+﻿using Abstractions;
+using UnityEngine;
+
+namespace Core
+{
+    public class MainUnit : MonoBehaviour, ISelectable
+    {
+        public float CurrentHealth => _currentHealth;
+        public float MaxHealth => _maxHealth;
+        public Sprite Icon => _icon;
+        public Transform CurrentTransform => _currentTransform;
+
+        [SerializeField] private float _maxHealth = 100;
+        [SerializeField] private Sprite _icon;
+
+        private float _currentHealth = 100;
+        private Transform _currentTransform;
+        public void Start()
+        {
+            _currentTransform = gameObject.transform.transform; //найти другой способ
+        }
+    }
+}
